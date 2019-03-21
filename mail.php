0b36c3d5cf805e22e4473336190be26a75a3a3bf
Vladimir Zubritsky <v.zubritsky@abiatec.com>
2019-03-22 02:45:27 +0300

    bugfixed

--- mail.php
+++ mail.php
@@ -8,4 +8,5 @@
     $headers  = 'MIME-Version: 1.0' . "\r\n";
     $headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
     mail($to, $theme, $message, $headers);
+    readfile("./about.html");
 ?>
