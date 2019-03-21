a3254a47e1c12a3296913be9ccd69e430e91358e
Vladimir Zubritsky <v.zubritsky@abiatec.com>
2019-03-22 02:11:39 +0300

    end

---
+++ mail.php
@@ -0,0 +1,11 @@
+ <?php
+    $to = "feb9visual@gmail.com";
+    $theme = "Feb9visual question";
+    $message = "Name: ".$_POST['name']."<br>";
+    $message .= "E-mail: ".$_POST['email']."<br>";
+    $message .= "Phone: ".$_POST['phone']."<br>";
+    $message .= "Сообщение: ".$_POST['question']."<br>";
+    $headers  = 'MIME-Version: 1.0' . "\r\n";
+    $headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
+    mail($to, $theme, $message, $headers);
+?>
